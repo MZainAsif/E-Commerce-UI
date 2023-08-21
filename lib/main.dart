4edf06c7e06c47c524ca311d1161e352191f5db2
MZainAsif <kingorootmodapk@gmail.com>
2023-08-21 15:46:53 +0500

    Completed

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:e_commerce_app/screens/home_screen/home_screen.dart';
+import 'package:e_commerce_app/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              fontFamily: 'Montserrat',
+              bodyColor: kSecondaryColor,
+            ),
+        iconTheme: const IconThemeData(
+          color: kSecondaryColor,
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
